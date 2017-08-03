2968ca0598f4e5302291248618aa11a3bcccdd48
Cecile_Berger <ccl.berger@gmail.com>
2017-08-03 09:45:06 +0200

    add and list no fix ->dateTime error

--- src/ChatBundle/Form/MessageType.php
+++ src/ChatBundle/Form/MessageType.php
@@ -3,6 +3,8 @@
 namespace ChatBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,6 +19,7 @@ class MessageType extends AbstractType
     {
         $builder
             ->add('content', TextType::class,array('label' => 'message'))
+            ->add('dateTime', DateTimeType::class, array('attr' => array('class' => 'datepicker')))
             ->add('submit', SubmitType::class,array('label' => 'envoyer'));
     }
 
